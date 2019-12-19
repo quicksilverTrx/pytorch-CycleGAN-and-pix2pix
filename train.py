019390aaf5c174b2575c8b7457a7b411dc684544
Sohom chakraborty <quicksilverTrx@users.noreply.github.com>
2019-12-19 13:04:56 +0530

    Update train.py

--- train.py
+++ train.py
@@ -36,6 +36,7 @@ if __name__ == '__main__':
     total_iters = 0                # the total number of training iterations
 
     for epoch in range(opt.epoch_count, opt.n_epochs + opt.n_epochs_decay + 1):    # outer loop for different epochs; we save the model by <epoch_count>, <epoch_count>+<save_latest_freq>
+        print(epoch)
         epoch_start_time = time.time()  # timer for entire epoch
         iter_data_time = time.time()    # timer for data loading per iteration
         epoch_iter = 0                  # the number of training iterations in current epoch, reset to 0 every epoch
